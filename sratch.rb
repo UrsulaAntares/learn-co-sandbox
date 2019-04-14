79bbd58a29b99c7174be8babbb0a1e836eedabdf
Ursula Miller <ursula.miller+fi@gmail.com>
2019-04-14 18:54:00 +0000

    Automatically backed up by Learn

---
+++ sratch.rb
@@ -0,0 +1,12 @@
+nested_students = [
+  ["Mike", "Grade 10", "A average"],
+  ["Tim", "Grade 10", "C average"],
+  ["Monique", "Grade 11", "B average", "Class President"]
+]
+
+
+#def say_hi(name)
+#  puts "Yo ".upcase + name
+#end
+
+#say_hi("Tobias".upcase)
